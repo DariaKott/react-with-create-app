b5f66f37b439ad830004444c5c41769b8cc3ad89
Daria Kot <poturayko@gmail.com>
2023-10-09 12:45:58 +0200

    First commit, page with chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
+
+const data = [
+  ["Confesion", "Percent of population"],
+  ["Eastern Orthodoxy", 72],
+  ["Islamic", 19.11],
+  ["Catholics", 3.44],
+  ["Others", 4.14],
+  ["Atheist", 1.31]
+];
+
+const options = {
+  title: "Religions in Montenegro",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
     </div>
   );
 }
